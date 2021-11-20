ffd0488f57bca4ae5c2fd44860fce1ae12d50016
paul <paul.surrans@epitech.eu>
2021-11-20 13:16:10 +0100

    delete key

--- api/rescuer/settings.py
+++ api/rescuer/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wx+98c)_u6n#==^g*hy%llq!xp3lt#$og^5sfs2$5yj4)2sfbr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
