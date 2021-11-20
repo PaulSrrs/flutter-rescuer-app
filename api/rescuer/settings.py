01da4d0f760031c1fe8e6ee4f16adbd3bd01f725
paul <paul.surrans@epitech.eu>
2021-11-20 13:19:10 +0100

    change key

--- api/rescuer/settings.py
+++ api/rescuer/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-w=t00k4fnaytfz9uy6#y-ovs&3n0umxhr9729vk*bzg6(f-mvq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
